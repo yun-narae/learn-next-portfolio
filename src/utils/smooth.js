604352cd26a83a027620ff7d3ad0cfdf6214dcb7
seulbinim <skfo0827@naver.com>
2025-02-07 16:51:22 +0900

    스무스 효과 해결 완료

--- src/utils/smooth.js
+++ src/utils/smooth.js
@@ -2,28 +2,28 @@ import Lenis from "@studio-freight/lenis";
 import debounce from "lodash.debounce"; // lodash.debounce 불러오기
 
 const smooth = () => {
-    const smooth = new Lenis({
-        duration: 1.5, // 좀 더 느리게, 스크롤 시간이 길어짐
-        easing: (t) => Math.min(1, 1.8 - Math.pow(2, -10 * t)), // 더 강렬한 easing 효과
-        smoothWheel: true, // 마우스 휠의 스크롤을 부드럽게 처리
-        smoothTouch: true, // 터치 스크롤도 부드럽게 처리
+    const lenis = new Lenis({
+        duration: 1,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        direction: "vertical",
+        gestureDirection: "vertical",
+        smooth: true,
     });
 
     function raf(time) {
-        smooth.raf(time);
+        lenis.raf(time);
         requestAnimationFrame(raf);
     }
 
     requestAnimationFrame(raf);
 
-    // 스크롤 이벤트 디바운스 처리
-    const handleScroll = debounce((e) => {
-        console.log("Smooth Scroll Event:", e);
-        // 스크롤 위치를 화면에 표시 (디버깅용)
-        console.log("Scroll Position:", e.scroll);
-    }, 200); // 200ms 간격으로 이벤트 처리
-
-    smooth.on("scroll", handleScroll); // 디바운스된 스크롤 이벤트 핸들러 적용
+    // debounce 적용된 scroll 이벤트 핸들러
+    lenis.on(
+        "scroll",
+        debounce((e) => {
+            console.log(e);
+        }, 200) // 200ms 지연 후 실행
+    );
 };
 
 export default smooth;

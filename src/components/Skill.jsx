e61ec52af6a5b7b5c083de21f8d490feb461df41
seulbinim <skfo0827@naver.com>
2025-02-07 13:24:45 +0900

    스킬 완료

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { skillText } from "../constants"
 
 const Skill = () => {
-  return (
-    <div>Skill</div>
-  )
-}
+    return (
+        <section id="skill">
+            <div className="skill__inner">
+                <h2 className="skill__title">Challenge <em>나의 도전</em></h2>
+                <div className="skill__desc">
+                    {skillText.map((skill, key) => (
+                        <div key={key}>
+                            <h3><span>{key+1}.</span>{skill.title}</h3>
+                            <p>{skill.desc}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default Skill
+export default Skill;
